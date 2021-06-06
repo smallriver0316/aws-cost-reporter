8e8091c7062181d5c222986c6aaa8cbae984bd8c
smallriver0316 <masahiro@TECHNODROME.localdomain>
2021-06-06 15:21:42 +0900

    Succeeded to get cost and usage

--- src/aws/cost-explorer.js
+++ src/aws/cost-explorer.js
@@ -1,28 +1,52 @@
 'use strict';
 module.exports = class CostExplorer {
-    constructor(client) {
-        this.client = client;
-    }
+  constructor (client) {
+    this.client = client;
+  }
 
-    getCostCategories = async () => {
-        const params = {
-            TimePeriod: {
-                Start: '2021-05-01',
-                End: '2021-05-31',
-            }
-        };
-        return new Promise((resolve, reject) => {
-            return this.client.getCostCategories(params, (err, data) => {
-                if (err) {
-                    console.error('[CostExplorer][getCostCategories][Error] Failed to get cost categories');
-                    reject(err);
-                } else {
-                    console.log('[CostExplorer][getCostCategories] Succeeded to get cost categories');
-                    resolve(data);
-                }
-            });
-        });
-    }
+  getCostCategories = async () => {
+    const params = {
+      TimePeriod: {
+        Start: '2021-05-01',
+        End: '2021-05-31'
+      }
+    };
+    return new Promise((resolve, reject) => {
+      return this.client.getCostCategories(params, (err, data) => {
+        if (err) {
+          console.error('[CostExplorer][getCostCategories][Error] Failed to get cost categories');
+          reject(err);
+        } else {
+          console.log('[CostExplorer][getCostCategories] Succeeded to get cost categories');
+          resolve(data);
+        }
+      });
+    });
+  }
 
-    // getCostAndUsage = () => {}
-}
+  getCostAndUsage = async (startDate, endDate) => {
+    const params = {
+      Granularity: 'MONTHLY',
+      Metrics: [
+        'BlendedCost',
+        'AmortizedCost',
+        'UsageQuantity'
+      ],
+      TimePeriod: {
+        Start: startDate,
+        End: endDate
+      }
+    };
+    return new Promise((resolve, reject) => {
+      return this.client.getCostAndUsage(params, (err, data) => {
+        if (err) {
+          console.error('[CostExplorer][getCostAndUsage][Error] Failed to get cost and usage');
+          reject(err);
+        } else {
+          console.log('[CostExplorer][getCostAndUsage] Succeeded to get cost and usage');
+          resolve(data);
+        }
+      });
+    });
+  }
+};
--- src/lambda/handler.js
+++ src/lambda/handler.js
@@ -1,20 +1,23 @@
 'use strict';
-var AWS = require('aws-sdk');
-var CostExplorer = require('../aws/cost-explorer');
+const AWS = require('aws-sdk');
+const CostExplorer = require('../aws/cost-explorer');
+const moment = require('moment');
 
-var ceClient = new AWS.CostExplorer({
-  apiVersion: '2017-12-25',
+const ceClient = new AWS.CostExplorer({
+  apiVersion: '2017-12-25'
 });
 
 module.exports.costReporter = async (event, context, callback) => {
   try {
+    const startDate = moment().startOf('month').format('YYYY-MM-DD');
+    const endDate = moment().format('YYYY-MM-DD');
     const costExplorer = new CostExplorer(ceClient);
-    const ret = await costExplorer.getCostCategories();
+    const ret = await costExplorer.getCostAndUsage(startDate, endDate);
     callback(null, {
       statusCode: 200,
-      body: JSON.stringify(ret),
+      body: JSON.stringify(ret)
     });
-  } catch(err) {
+  } catch (err) {
     callback(err);
   }
 };
