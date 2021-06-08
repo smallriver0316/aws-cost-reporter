2394962ff4c2946c72ab3f51e50c7cd3a320f10c
smallriver0316 <masahiro@TECHNODROME.localdomain>
2021-06-08 23:03:11 +0900

    Implemented SNS publishing

--- src/lambda/handler.js
+++ src/lambda/handler.js
@@ -1,21 +1,30 @@
 'use strict';
 const AWS = require('aws-sdk');
 const CostExplorer = require('../aws/cost-explorer');
+const SNS = require('../aws/sns');
 const moment = require('moment');
 
 const ceClient = new AWS.CostExplorer({
   apiVersion: '2017-12-25'
+});
+const snsClient = new AWS.SNS({
+  apiVersion: '2010-03-31'
 });
 
 module.exports.costReporter = async (event, context, callback) => {
   try {
     const startDate = moment().startOf('month').format('YYYY-MM-DD');
     const endDate = moment().format('YYYY-MM-DD');
+
     const costExplorer = new CostExplorer(ceClient);
-    const ret = await costExplorer.getCostAndUsage(startDate, endDate);
+    const ceRet = await costExplorer.getCostAndUsage(startDate, endDate);
+
+    const sns = new SNS(snsClient);
+    const snsRet = await sns.publish(JSON.stringify(ceRet));
+
     callback(null, {
       statusCode: 200,
-      body: JSON.stringify(ret)
+      body: JSON.stringify(snsRet)
     });
   } catch (err) {
     callback(err);
