1a4d7dc8f9f26f263aa1cf995445969e52e1abc1
smallriver0316 <masahiro@TECHNODROME.localdomain>
2021-06-10 22:20:13 +0900

    Added template for lambda invocation event

--- src/lambda/handler.js
+++ src/lambda/handler.js
@@ -11,6 +11,25 @@ const snsClient = new AWS.SNS({
   apiVersion: '2010-03-31'
 });
 
+function createMessage (report) {
+  let msg = 'Failed to create message.';
+  if (report.ResultsByTime) {
+    const result = report.ResultsByTime[0];
+    const startDate = result.TimePeriod.Start;
+    const endDate = result.TimePeriod.End;
+    let totalCost = '';
+    if (result.Total.BlendedCost) {
+      totalCost = `${result.Total.BlendedCost.Amount} ${result.Total.BlendedCost.Unit}`;
+    }
+    let usageQuantity = '';
+    if (result.Total.UsageQuantity) {
+      usageQuantity = result.Total.UsageQuantity;
+    }
+    msg = `From ${startDate} to ${endDate}\nCurrent Cost in this month: ${totalCost}\nUsage Quantity: ${usageQuantity}`;
+  }
+  return msg;
+}
+
 module.exports.costReporter = async (event, context, callback) => {
   try {
     const startDate = moment().startOf('month').format('YYYY-MM-DD');
@@ -18,9 +37,12 @@ module.exports.costReporter = async (event, context, callback) => {
 
     const costExplorer = new CostExplorer(ceClient);
     const ceRet = await costExplorer.getCostAndUsage(startDate, endDate);
+    console.log(JSON.stringify(ceRet));
+
+    const msg = createMessage(ceRet);
 
     const sns = new SNS(snsClient);
-    const snsRet = await sns.publish(JSON.stringify(ceRet));
+    const snsRet = await sns.publish(msg);
 
     callback(null, {
       statusCode: 200,
